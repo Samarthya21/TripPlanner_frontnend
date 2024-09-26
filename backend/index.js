38b6a17ca92f80fbbd7c7e504802c7d27e7f5e4e
Samarthya21 <samarthya777@gmail.com>
2024-09-26 16:18:37 +0530

    updated cors policy

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const user_model = require('./schema.js');
 const loginroute = require('./components/loginroute.js');
 const signuproute = require('./components/signuproute.js');
 const saveTrip = require('./components/saveTrip.js');
@@ -9,23 +8,32 @@ const myTrips = require('./components/myTrips.js');
 const app = express();
 const port = 8000;
 
+// Allow CORS for all routes and methods
+const allowedOrigins = [
+    'https://trip-planner-frontnend-d8h6fow0e-samarthya21s-projects.vercel.app'
+];
+
 app.use(cors({
-    origin: 'https://trip-planner-frontnend-d8h6fow0e-samarthya21s-projects.vercel.app/', 
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-   
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    
 }));
+
 app.use(express.json());
+
+
+loginroute(app);
+signuproute(app);
+saveTrip(app);
+myTrips(app);
+
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
-
-
-
-loginroute(app);
-
-signuproute(app);
- 
-saveTrip(app);
-
-myTrips(app);
